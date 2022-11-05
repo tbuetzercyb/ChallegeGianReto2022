2a40c8edce44b117a986528ee046d09aef0e0d37
lavinia <lmorett@student.ethz.ch>
2022-11-05 20:59:55 +0100

    ciao

--- app/src/main/java/com/example/sbb_hackhealth/MainActivity.kt
+++ app/src/main/java/com/example/sbb_hackhealth/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.sbb_hackhealth
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.Global.getString
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -10,12 +17,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.NotificationManagerCompat
 import com.example.sbb_hackhealth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val CHANNEL_ID = "important"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -30,10 +40,29 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        var i = 0
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAnchorView(R.id.fab)
-                    .setAction("Action", null).show()
+            val newMessageNotification = Notification.Builder(view.context, "important")
+                .setSmallIcon(androidx.appcompat.R.drawable.abc_dialog_material_background)
+                .setContentTitle(getString(R.string.pushNotificationTitle))
+                .setContentText(getString(R.string.pushNotificationContent))
+//                .addAction(action)
+                .build()
+
+// Issue the notification.
+            with(NotificationManagerCompat.from(this)) {
+                notificationManager.notify(i++, newMessageNotification)
+            }
         }
     }
 
@@ -57,5 +86,20 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    private fun createNotificationChannel(str: channel_names, ){
+        val name = getString(channel_names)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+
+
     }
 }
